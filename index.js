67629d3d0f0cc0c29b929b600f16b56e3df9e623
Alexei Palacios <sukesasuk2010@gmail.com>
2023-09-08 19:52:07 -0600

    [ADD] Cors options

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("./db");
+const bodyParser = require('body-parser')
 const morgan = require('morgan');
 const cors = require("cors");
 const mongoose = require('mongoose');
@@ -7,6 +8,14 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const app = express();
 const port = process.env.PORT;
+
+var corsOptions = {
+    origin: "https://cripto-mocha.vercel.app/"
+  };
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json({
+    limit: '50mb'
+  }))
 
 app.use(morgan('dev'));
 
