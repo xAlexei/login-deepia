12e207d6a6ec631c4021882e6e0cc393f8fcd93b
Alexei Palacios <sukesasuk2010@gmail.com>
2023-09-13 19:45:08 -0600

    Cors

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ var corsOptions = {
     limit: '50mb'
   }))
 
+  app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(morgan('dev'));
 
 db.connect();
